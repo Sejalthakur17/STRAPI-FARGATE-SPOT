bd327f809fd2698b5a9f3d005073652b52aae495
Sejal Thakur <sejalthakurcs@gmail.com>
2026-02-20 14:49:39 +0530

    Adding ALB

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -3,18 +3,36 @@ resource "aws_db_subnet_group" "db_subnet_group" {
   subnet_ids = data.aws_subnets.default.ids
 }
 
+resource "aws_security_group" "rds_sg" {
+  name        = "sejal-rds-sg"
+  description = "Allow ECS to connect to RDS"
+  vpc_id      = data.aws_vpc.selected.id
 
+  ingress {
+    from_port       = 5432
+    to_port         = 5432
+    protocol        = "tcp"
+    security_groups = [aws_security_group.ecs_sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 resource "aws_db_instance" "sejal_db" {
   identifier              = "sejal-db"
   engine                  = "postgres"
   instance_class          = "db.t3.micro"
   allocated_storage       = 20
-  db_name                 = "strapi" 
+  db_name                 = "strapi"
   username                = var.db_username
   password                = var.db_password
   skip_final_snapshot     = true
   multi_az                = false
   publicly_accessible     = false
   db_subnet_group_name    = aws_db_subnet_group.db_subnet_group.name
-  vpc_security_group_ids  = [aws_security_group.ecs_sg.id]
+  vpc_security_group_ids  = [aws_security_group.rds_sg.id]
 }
